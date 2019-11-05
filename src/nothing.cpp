4cfdac0b669004df92da278ac36b4a8a407ad40f
xanderdj <x.de.j@hotmail.com>
2019-11-05 21:52:52 +0100

    added template

---
+++ src/nothing.cpp
@@ -0,0 +1,23 @@
+/**
+ * Nothing
+ *
+ * Does nothing but is a template for arduino
+ */
+
+#include "Arduino.h"
+
+/**
+ * Initialisation code for arduino
+*/
+void setup()
+{
+
+}
+
+/**
+ * Function that will be called over and over
+ * Has local scope any variable declared in the function will be lost next time loop is called
+*/
+void loop(){
+
+}
