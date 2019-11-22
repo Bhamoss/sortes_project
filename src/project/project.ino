5ccb611bc5daa473f6b5ae6e82e77c00564d5158
Bhamoss <26162072+Bhamoss@users.noreply.github.com>
2019-11-22 14:37:44 +0100

    voor meeting

---
+++ src/project/project.ino
@@ -0,0 +1,16 @@
+// Our frequency 869700000
+
+
+
+// Include Arduino FreeRTOS library
+#include <Arduino_FreeRTOS.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
